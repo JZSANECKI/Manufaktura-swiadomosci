2b038eb94effe893b3f6d307a876c4bd360088dc
JZSANECKI <115653252+JZSANECKI@users.noreply.github.com>
2022-11-20 01:51:40 +0100

    3 files modified

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 let wysokosc = window.innerHeight;
 let szerokosc = window.innerWidth;
+let divElement = document.querySelector(".main");
+let elemHeight = divElement.clientHeight;
 window.addEventListener('scroll', function () {
   let value = window.scrollY;
 
   heroimg.style.top = 0 - value *0.2+ 'px';
   heroimgcloud.style.top = 0 - value * 0.1 + 'px';
   
-  main.style.top =  wysokosc - (0.8 * value)  + 'px';
-
- 
+  main.style.top =  wysokosc - ((elemHeight/wysokosc) * value)  + 'px';
+// breakespace.style.height = wysokosc - elemHeight + 'px';
+console.log (elemHeight);
 });
