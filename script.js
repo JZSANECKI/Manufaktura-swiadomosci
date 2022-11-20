1430f3c58c5938b78f22ccac0755a23c0a5c0366
JZSANECKI <115653252+JZSANECKI@users.noreply.github.com>
2022-11-20 20:51:05 +0100

    3 files modified

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
 let wysokosc = window.innerHeight;
 let szerokosc = window.innerWidth;
-let divElement = document.querySelector(".main");
+let divElement = document.querySelector('.main');
 let elemHeight = divElement.clientHeight;
+
 window.addEventListener('scroll', function () {
   let value = window.scrollY;
-
-  heroimg.style.top = 0 - value *0.2+ 'px';
+  let navWys = (wysokosc / value);
+  heroimg.style.top = 0 - value * 0.2 + 'px';
   heroimgcloud.style.top = 0 - value * 0.1 + 'px';
+
+  main.style.top = wysokosc - (elemHeight / wysokosc) * value + 'px';
+  // breakespace.style.height = wysokosc - elemHeight + 'px';
+  console.log(navWys);
   
-  main.style.top =  wysokosc - ((elemHeight/wysokosc) * value)  + 'px';
-// breakespace.style.height = wysokosc - elemHeight + 'px';
-console.log (elemHeight);
+  if (navWys < 3) {
+    navbar.style.background = 'rgba(255,255,255,0.9)';
+    navbar.classList.add("shadow");
+    
+    console.log('ciemny nav');
+  } else {
+    navbar.style.background = 'rgba(255,255,255,0.3)';
+    navbar.classList.remove("shadow");
+    console.log('biały nav');
+    
+  }
 });
