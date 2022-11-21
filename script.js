2480d9c528bd715124aeb42e59671f77ef343536
JZSANECKI <115653252+JZSANECKI@users.noreply.github.com>
2022-11-21 16:00:21 +0100

    2 files modified

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ window.addEventListener('scroll', function () {
   let navWys = (wysokosc / value);
   heroimg.style.top = 0 - value * 0.2 + 'px';
   heroimgcloud.style.top = 0 - value * 0.1 + 'px';
-
+quotes.style.filter = "blur(" + (1/(wysokosc / (50* value))) +"px)"
   main.style.top = wysokosc - (elemHeight / wysokosc) * value + 'px';
   // breakespace.style.height = wysokosc - elemHeight + 'px';
   
