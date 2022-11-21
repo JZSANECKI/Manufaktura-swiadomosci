c5a999c3d3bbb33a68be4163dce4826de29fd75b
JZSANECKI <115653252+JZSANECKI@users.noreply.github.com>
2022-11-21 23:46:53 +0100

    1 file modified

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ let navlinkA = document.getElementsByClassName('nav-link');
 window.addEventListener('scroll', function () {
   let value = window.scrollY;
   let navWys = (wysokosc / value);
-  heroimg.style.top = 0 - value * 0.2 + 'px';
+  heroimg.style.top = 0 - value * 0.3 + 'px';
   heroimgcloud.style.top = 0 - value * 0.1 + 'px';
 quotes.style.filter = "blur(" + (1/(wysokosc / (50* value))) +"px)"
   main.style.top = wysokosc - (elemHeight / wysokosc) * value + 'px';
