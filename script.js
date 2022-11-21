2f3a6fa1ea00eb6e1307ab0819ddec7b9430bc37
JZSANECKI <115653252+JZSANECKI@users.noreply.github.com>
2022-11-21 15:37:48 +0100

    3 files modified

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ let logo = document.querySelector('.logo');
 let elemHeight = divElement.clientHeight;
 let navlinkA = document.getElementsByClassName('nav-link');
 
+
 window.addEventListener('scroll', function () {
   let value = window.scrollY;
   let navWys = (wysokosc / value);
@@ -13,24 +14,29 @@ window.addEventListener('scroll', function () {
 
   main.style.top = wysokosc - (elemHeight / wysokosc) * value + 'px';
   // breakespace.style.height = wysokosc - elemHeight + 'px';
-  console.log(navWys);
+  console.log(navlinkB);
   
   if (navWys < 3) {
     navbar.style.background = 'rgba(255,255,255,0.9)';
+    
     navbar.classList.add("shadow");
     navlinkA[0].style.color = 'black';
     navlinkA[0].onmouseover = function(){navlinkA[0].style.color = 'gray'};
     navlinkA[0].onmouseout = function(){navlinkA[0].style.color = 'black'};
     navlinkA[1].style.color = 'black';
+    
     navlinkA[1].onmouseover = function(){navlinkA[1].style.color = 'gray'};
     navlinkA[1].onmouseout = function(){navlinkA[1].style.color = 'black'};
     navlinkA[2].style.color = 'black';
+    
     navlinkA[2].onmouseover = function(){navlinkA[2].style.color = 'gray'};
     navlinkA[2].onmouseout = function(){navlinkA[2].style.color = 'black'};
     navlinkA[3].style.color = 'black';
+    
     navlinkA[3].onmouseover = function(){navlinkA[3].style.color = 'gray'};
     navlinkA[3].onmouseout = function(){navlinkA[3].style.color = 'black'};
     navlinkA[4].style.color = 'black';
+    
     navlinkA[4].onmouseover = function(){navlinkA[4].style.color = 'gray'};
     navlinkA[4].onmouseout = function(){navlinkA[4].style.color = 'black'};
     
@@ -39,6 +45,8 @@ window.addEventListener('scroll', function () {
   } else {
     navbar.style.background = 'rgba(255,255,255,0.2)';
     navbar.classList.remove("shadow");
+    
+    navbarNavAltMarkup.style.backgroundColor = 'rbga(0.0.0.0.8)';
     navlinkA[0].style.color = 'white';
     navlinkA[0].onmouseover = function(){navlinkA[0].style.color = 'lightgray'};
     navlinkA[0].onmouseout = function(){navlinkA[0].style.color = 'white'};
